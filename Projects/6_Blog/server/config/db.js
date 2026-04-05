213ef9cac7ed45f0d22bb24d0d910ac35d152ea8
_jinal__rathod_ <jinalrathod594@gmail.com>
2026-04-05 22:35:14 +0530

    p-6

--- Projects/6_Blog/server/config/db.js
+++ Projects/6_Blog/server/config/db.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config({
-  path: "./config/config.env",
+  path: "./.env",
 });
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://jinalrathod594_db_user:jinal_594@cluster0.qwbepcd.mongodb.net/");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
     console.log("DB Error:", error);
